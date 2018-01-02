3cb94c9764332018ead62093d5eeaa202fd25465
harshil-bacancy <harshil.patel@bacancytechnology.com>
2018-01-02 12:28:10 +0530

    Project Added

---
+++ Call/src/com/example/call/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.call;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		Uri number = Uri.parse("tel:" + "9898989898");
+		Intent intent = new Intent(Intent.ACTION_CALL, number);
+		startActivity(intent);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}

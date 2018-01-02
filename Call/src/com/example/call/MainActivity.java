4b9ff905295bda0356dac992701e04dab78ea08b
jinal-Bacancy <jinal.vandara@bacancytechnology.com>
2018-01-02 12:50:28 +0530

    change in MainActivity

--- Call/src/com/example/call/MainActivity.java
+++ Call/src/com/example/call/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		Uri number = Uri.parse("tel:" + "9898989898");
+		Uri number = Uri.parse("tel:" + "8888888888");
 		Intent intent = new Intent(Intent.ACTION_CALL, number);
 		startActivity(intent);
 	}
